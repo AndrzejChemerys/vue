f9f360de1007d3fec81292b0f4eba1ba9899a36f
AndriiChemerys <andriichemerys@gmail.com>
2020-05-05 21:31:54 +0200

    Components (Pt. 2)

--- vue_courses/1_vue_mastery_courses/3_beautify_with_vuetify/les1/app/src/App.vue
+++ vue_courses/1_vue_mastery_courses/3_beautify_with_vuetify/les1/app/src/App.vue
@@ -3,27 +3,16 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn text rounded to="/">Home</v-btn>
+      <v-btn 
+      v-for = "link in links"
+      :key = "`${link.label}-header-link`"
+      text 
+      rounded 
+      to="link.url">{{link.label}}</v-btn>
     </v-app-bar>
     <v-content>
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
-            <v-text-field :type="showPassword ? 'text' : 'password'" label="Password" prepend-icon="mdi-lock" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click:append="showPassword = !showPassword"></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
     
 <!--  -->
@@ -38,7 +27,7 @@
       >
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link-label}-footer-link`"
           color="white"
           text
           rounded
@@ -66,8 +55,7 @@ export default {
   components: {},
 
   data: () => ({
-    showPassword: false,
-    links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"],
+    links: [{ label: "Home", url: "/" }, { label: "Login", url: "/login" }],
   }),
 };
 </script>
