6177ddd77efe4a64a91320f99c1c61c1de489f4d
AndriiChemerys <andriichemerys@gmail.com>
2020-05-13 20:23:10 +0200

    Events

--- vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec2/les5/app.js
+++ vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec2/les5/app.js
@@ -2,15 +2,11 @@ new Vue({
     el: '#app',
     data: {
         title: 'Becoming a Vue ninja',
-        name: 'Ryu',
-        url: "http://www.youtube.com",
-        classes: ['one', 'two']
-
+        wage: 0
     },
     methods: {
-        greet(time) {
-            this.title
-            return `Hello and good ${time}, ${this.name}`
+        changeWage(amount) {
+            this.wage += amount;
         }
     }
 })
