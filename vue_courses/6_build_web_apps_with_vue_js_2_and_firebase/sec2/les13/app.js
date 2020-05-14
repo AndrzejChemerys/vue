151bae554c099846ee8ad745246cf98c21226daf
AndriiChemerys <andriichemerys@gmail.com>
2020-05-14 20:34:03 +0200

    Modifiers

---
+++ vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec2/les13/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Becoming a Vue ninja',
+        name: ''
+    },
+    methods: {
+        logMessage() {
+            console.log('Hello world')
+        }
+
+    }
+})
