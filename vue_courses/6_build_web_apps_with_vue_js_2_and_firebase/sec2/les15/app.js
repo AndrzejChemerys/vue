4b791810aec7912947208bec5e59e3ec8e6fbf8a
AndriiChemerys <andriichemerys@gmail.com>
2020-05-14 21:01:14 +0200

    Looping with v-for

---
+++ vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec2/les15/app.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Becoming a Vue ninja',
+        items: ['Mushroom', 'Green Shells', 'Red Shells', 'Banana', 'Star'],
+        ninjas: [{
+                name: 'Crystal',
+                age: 25,
+                belt: 'black'
+            },
+            {
+                name: 'Ryu',
+                age: 30,
+                belt: 'brown'
+            },
+            {
+                name: 'Ken',
+                age: 35,
+                belt: 'orange'
+            },
+        ]
+    },
+    methods: {
+
+    }
+})
