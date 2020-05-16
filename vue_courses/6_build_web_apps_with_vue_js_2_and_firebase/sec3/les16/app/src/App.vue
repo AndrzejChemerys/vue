8e785a90adbf75b5c9ece9ef38bc7e901fb9e255
AndriiChemerys <andriichemerys@gmail.com>
2020-05-16 11:25:01 +0200

    Making Requests with Axios

--- vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec3/les16/app/src/App.vue
+++ vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec3/les16/app/src/App.vue
@@ -1,43 +1,25 @@
 <template>
   <div id="app">
-    <h1>{{title}}</h1>
+    <h1>ASD</h1>
     <Navbar/>
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends"/>
+    <Blogs/>
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
-import AllFriends from "./AllFriends";
-import OnlineFriends from "./OnlineFriends";
+import Blogs from "./Blogs";
 
 export default {
   name: "app",
   components: {
     Navbar,
-    OnlineFriends,
-    AllFriends,
+    Blogs,
   },
   data() {
-    return {
-      title: "My not first Vue.js App",
-      friends: [
-        { name: "Mario", online: true },
-        { name: "Luigi", online: false },
-        { name: "Toad", online: true },
-        { name: "Bowser", online: false },
-      ],
-    };
+    return {};
   },
-  methods: {
-    deleteFriend(payload) {
-      console.log(payload);
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name;
-      });
-    },
-  },
+  methods: {},
 };
 </script>
 
