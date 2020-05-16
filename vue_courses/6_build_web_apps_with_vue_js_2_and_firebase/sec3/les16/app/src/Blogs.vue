175e38060879f0c205faacb36c076061ecaaa8f4
AndriiChemerys <andriichemerys@gmail.com>
2020-05-16 11:43:15 +0200

    Making Requests with Axios

--- vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec3/les16/app/src/Blogs.vue
+++ vue_courses/6_build_web_apps_with_vue_js_2_and_firebase/sec3/les16/app/src/Blogs.vue
@@ -1,34 +1,48 @@
 <template>
-    <div class="blogs">
-        <h2>{{blogTitle}}</h2>
-        <button @click="changeTitle">Change Title</button>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
     </div>
+    <button @click="changeTitle">Change Title</button>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Blogs",
   data() {
     return {
-      blogTitle: "Blogs",
+      blogTitle: "Blogs"
     };
   },
   methods: {
     changeTitle() {
       this.blogTitle = "Amazing Blog Site";
-    },
+    }
   },
   beforeCreate() {
     alert("beforeCreate hook");
   },
   created() {
     alert("created hook");
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/")
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   beforeUpdate() {
     alert("beforeUpdate");
-  },
+  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
