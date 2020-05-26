f3aacaee8b8376040f606c28e2b895aaa39461f8
AndriiChemerys <andriichemerys@gmail.com>
2020-05-26 21:34:06 +0200

    Transitions

---
+++ vue_courses/1_vue_mastery_courses/4_animating_vue/animating-vue-lesson2-START/src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <button @click="toggleModal">Open</button>
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen
+    },
+  },
+}
+</script>
+
+<style>
+body {
+  background-color: rgb(87, 87, 87);
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
