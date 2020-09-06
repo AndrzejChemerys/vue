32ef0c7404f81399760d6268ff388b17e3a575b3
AndriiChemerys <andriichemerys@gmail.com>
2020-09-06 11:07:38 +0200

    Event Handling

--- vue_courses/1_vue_mastery_courses/5_intro_to_vue_3/Intro-to-Vue-3-L5-start/main.js
+++ vue_courses/1_vue_mastery_courses/5_intro_to_vue_3/Intro-to-Vue-3-L5-start/main.js
@@ -5,7 +5,30 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            sizes: ['S', 'M', 'L', "XL"]
+            sizes: ['S', 'M', 'L', "XL"],
+            variants: [{
+                    id: 2234,
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg'
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg'
+                },
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart -= 1
         }
     }
 })
