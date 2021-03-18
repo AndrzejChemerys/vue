a3463919715bf6e3081b6181a5e7c61a75ab08a6
AndriiChemerys <andriichemerys@gmail.com>
2021-03-18 18:28:12 +0100

    Outputting Lists (v-for)

--- vue_courses/7_build_web_apps_with_vue_js_3_and_firebase/sec1/les1/app.js
+++ vue_courses/7_build_web_apps_with_vue_js_3_and_firebase/sec1/les1/app.js
@@ -7,12 +7,29 @@ const app = Vue.createApp({
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45,
-            showBooks: true
+            showBooks: true,
+            books: [
+                {title: 'man of the wind', author: 'patrick rothfuss'},
+                {title: 'the way of kings', author: 'brandon sanderson'},
+                {title: 'the final empire', author: 'patrick rothfuss'},
+            ],
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 });
