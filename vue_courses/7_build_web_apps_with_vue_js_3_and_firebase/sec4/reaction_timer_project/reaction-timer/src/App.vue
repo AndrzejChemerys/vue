a5ffb10ab514f346e036f00b640f1f7f9ad935eb
AndriiChemerys <andriichemerys@gmail.com>
2021-04-05 18:36:22 +0200

    Showing a Results Component

--- vue_courses/7_build_web_apps_with_vue_js_3_and_firebase/sec4/reaction_timer_project/reaction-timer/src/App.vue
+++ vue_courses/7_build_web_apps_with_vue_js_3_and_firebase/sec4/reaction_timer_project/reaction-timer/src/App.vue
@@ -2,15 +2,17 @@
   <h1>Raction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from "./components/Block.vue"
+import Results from "./components/Results";
 
 export default {
   name: 'App',
   components: {
+    Results,
     Block
   },
   data(){
