d662bb6f1e8de31d53757c83e30215750a72c30e
AndriiChemerys <andriichemerys@gmail.com>
2021-04-07 19:52:50 +0200

    Checkboxes

--- vue_courses/7_build_web_apps_with_vue_js_3_and_firebase/sec5/web-form/src/components/SignupForm.vue
+++ vue_courses/7_build_web_apps_with_vue_js_3_and_firebase/sec5/web-form/src/components/SignupForm.vue
@@ -11,19 +11,44 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+    
+    <div>
+      <input type="checkbox" value="Shaun" v-model="names">
+      <label>Shaun</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="Yoshi" v-model="names">
+      <label>Yoshi</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="Mario" v-model="names">
+      <label>Mario</label>
+    </div>
+    
   </form>
   <p>Email: {{email}}</p>
   <p>Password: {{password}}</p>
   <p>Role: {{role}}</p>
+  <p>Terms accepted: {{terms}}</p>
+  <p>Names: {{ names }} </p>
 </template>
 
 <script>
 export default {
   data(){
     return {
-      email: '',
+      email: 'mario',
       password: '',
-      role: ''
+      role: 'designer',
+      terms: false,
+      names: []
     }
   }
 }
@@ -56,4 +81,12 @@ export default {
     border-bottom: 1px solid #ddd;
     color: #555;
   }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+
 </style>
